8474e3a0a8441691b859d7d0267ca5d4834ab245
mengsu89 <mengsu@live.se>
2014-08-23 00:03:45 +0200

    Fixed the problem of the "admin" menu showing for users.

--- public/modules/admin/config/admin.client.config.js
+++ public/modules/admin/config/admin.client.config.js
@@ -4,7 +4,7 @@
 angular.module('admin').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Admin', 'admin', 'dropdown');
+		Menus.addMenuItem('topbar', 'Admin', 'admin', 'dropdown', undefined, false, ['admin']);
 		Menus.addSubMenuItem('topbar', 'admin', 'Hantera Homeparties', 'admin/requests');
 		Menus.addSubMenuItem('topbar', 'admin', 'Hantera Användare', 'admin/users');
 	}
