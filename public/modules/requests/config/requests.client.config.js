20df1033404723a4b026f1d3b8d4129e316a6eee
mengsu89 <mengsu@live.se>
2014-08-23 00:06:23 +0200

    "Create Homeparty" has been moved to the "admin" menu and thus has been removed from the "requests" module.

--- public/modules/requests/config/requests.client.config.js
+++ public/modules/requests/config/requests.client.config.js
@@ -4,9 +4,7 @@
 angular.module('requests').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Homeparties', 'requests', 'dropdown');
-		Menus.addSubMenuItem('topbar', 'requests', 'Alla Homeparties', 'requests');
-		Menus.addSubMenuItem('topbar', 'requests', 'Mina Homeparties', 'requests/myrequests');
-		Menus.addSubMenuItem('topbar', 'requests', 'Nytt Homeparty', 'requests/create', undefined, undefined);
+		Menus.addMenuItem('topbar', 'Förfrågningar', 'requests', 'item');
+		Menus.addMenuItem('topbar', 'Mina Homeparties', 'requests/myrequests', 'item');
 	}
 ]);
